2fdd20d31e7532060c39d5224747bb4c6c6f0e4c
meet <23bca301@vtcbcsr.edu.in>
2026-08-03 21:56:52 +0530

    AreaOfSquare

---
+++ 01-java-core/src/AreaOfSquare.java
@@ -0,0 +1,9 @@
+public class AreaOfSquare {
+	public static void main (String[] args) {
+		int side=4;
+		System.out.println("the area of square having side " +side+ " is "+(side*side));
+		System.out.println("the perimeter of square having side " +side+ " is "+(4*side));
+		
+	}
+
+}
