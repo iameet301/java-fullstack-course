22b475cdd6b45672d760248ebf324b99ae0c66c4
meet <23bca301@vtcbcsr.edu.in>
2026-08-06 19:08:09 +0530

    Different method of asign value using constructor

---
+++ 01-java-core/src/Emp.java
@@ -0,0 +1,21 @@
+
+public class Emp {
+	int id;
+	String name;
+	
+
+
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public Emp(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+}
---
+++ 01-java-core/src/EmpMain.java
@@ -0,0 +1,13 @@
+
+public class EmpMain {
+	public static void main(String[] args) {
+		Emp e=new Emp(1,"meet");
+		Emp ee=new Emp();
+		ee.id=2;
+		ee.name="kaushik";
+		System.out.println(e.id+" "+e.name);
+		System.out.println(ee.id+" "+ee.name);
+		
+		
+	}
+}
