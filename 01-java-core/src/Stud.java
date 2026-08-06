66f18aa7abf3f91cb1cd8c661c55ade395cd1509
meet <23bca301@vtcbcsr.edu.in>
2026-08-06 18:43:57 +0530

    constructor

--- 01-java-core/src/Stud.java
+++ 01-java-core/src/Stud.java
@@ -1,4 +1,12 @@
 
 public class Stud {
+	int id;
+	String name;
+	public Stud(int id, String name)
+	{
+		this.id=id;
+		this.name=name;
+		
+	}
 
 }
---
+++ 01-java-core/src/Studmain.java
@@ -0,0 +1,8 @@
+
+public class Studmain {
+	public static void main(String[] args) {
+		Stud s=new Stud(1,"meet");
+		System.out.println(s.id);
+		System.out.println(s.name);
+	}
+}
