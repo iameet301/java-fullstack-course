e2a16631e1acc74a2a4d43bead2515521d14ab4a
meet <23bca301@vtcbcsr.edu.in>
2026-08-10 19:01:49 +0530

    getter-setter

---
+++ 01-java-core/src/getter_setter/StudentMain.java
@@ -0,0 +1,12 @@
+package getter_setter;
+
+public class StudentMain {
+	public static void main(String[] args) {
+		Stuent s=new Stuent();
+		s.setId(1);
+		s.setName("meet");
+		System.out.println(s.getId()+" "+s.getName());
+		
+	}
+
+}
