4e2fe01bee761897f14cf56b8c0ad7dd4ebdbb52
meet <23bca301@vtcbcsr.edu.in>
2026-08-12 18:39:37 +0530

    cla

---
+++ 01-java-core/src/Exm.java
@@ -0,0 +1,11 @@
+
+public class Exm {
+	public static void main(String[] args) {
+		
+		System.out.println(args.length);
+		System.out.println(args[0]);
+		
+		String num=args[0];
+		int n=Integer.parseInt(num);
+	}
+}
