099ef07824e1801e7cef74d0d725062a7e497a83
meet <23bca301@vtcbcsr.edu.in>
2026-08-14 19:26:33 +0530

    Frustration

---
+++ 01-java-core/src/getter_setter/Emp.java
@@ -0,0 +1,34 @@
+package getter_setter;
+
+public class Emp {
+	
+	int  id;
+	String name;
+	public Emp(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Emp [id=" + id + ", name=" + name + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
