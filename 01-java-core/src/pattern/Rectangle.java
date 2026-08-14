1fe329bff326dc1d62a89eccd9d0c62d4929b52d
meet <23bca301@vtcbcsr.edu.in>
2026-08-14 18:25:44 +0530

    patterns

---
+++ 01-java-core/src/pattern/Rectangle.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class Rectangle {
+	public static void main(String[] args) {
+		int a=4;
+		int b=5;
+		for(int i=1; i<=a; i++) {
+			
+			for(int j=1; j<=b; j++) {
+				
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+}
