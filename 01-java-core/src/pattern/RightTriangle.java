a4f0bea96a798d3793de4eb99b8cbe762750e637
meet <23bca301@vtcbcsr.edu.in>
2026-08-19 12:27:21 +0530

    hh

---
+++ 01-java-core/src/pattern/RightTriangle.java
@@ -0,0 +1,15 @@
+package pattern;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            // Move to the next line after printing each row
+            System.out.println();
+        }
+    }
+}
