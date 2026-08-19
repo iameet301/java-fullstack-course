7e48d51a1cdf7d2a520e72507f90192267081352
meet <23bca301@vtcbcsr.edu.in>
2026-08-19 18:52:46 +0530

    check the number is positiv or nagative

---
+++ 01-java-core/src/P_N.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P_N {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Take a Number");
+		int l=sc.nextInt();
+		
+		if(l<0) {
+			System.out.println("number is nagativ");
+			
+		}
+		else {
+			System.out.println("number is positiv");
+		}
+		
+	}
+
+}
