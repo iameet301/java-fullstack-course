25b17b526295b4bf398b99fa1d3466f7ed10365b
meet <23bca301@vtcbcsr.edu.in>
2026-08-20 18:55:46 +0530

    vowel checking

---
+++ 01-java-core/src/Vowel.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Vowel {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("choose your character ");
+		char c=sc.next().charAt(0);
+		
+		if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+			System.out.println("its vowel");
+		else
+			System.out.println("its consonent");
+	}
+
+}
