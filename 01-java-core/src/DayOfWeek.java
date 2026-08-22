6abc9c429888b47ac072a67a22d6f0451b4052c9
meet <23bca301@vtcbcsr.edu.in>
2026-08-22 15:50:27 +0530

    day

---
+++ 01-java-core/src/DayOfWeek.java
@@ -0,0 +1,31 @@
+public class DayOfWeek {
+    public static void main(String[] args) {
+        int day = 3;
+        
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+            case 7: 
+                // Notice the intentional fall-through for the weekend
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Invalid day");
+                // No break needed in the final default case
+        }
+    }
+}
