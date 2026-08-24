15762d07aecb5d31218bb920528231b59c746314
meet <23bca301@vtcbcsr.edu.in>
2026-08-24 18:10:19 +0530

    ..

--- 01-java-core/src/Num_5.java
+++ 01-java-core/src/Num_5.java
@@ -6,9 +6,9 @@ public class Num_5 {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("enter a number");
 	int num=sc.nextInt();
-	if(num % 5==0)
-		System.out.println("divisible by 5");
+	if(num % 5==0 && num% 3==0)
+		System.out.println("divisible by 5 and 3");
 	else
-		System.out.println("not");
+		System.out.println("not divisible by 5 or 3");
 	}
 }
