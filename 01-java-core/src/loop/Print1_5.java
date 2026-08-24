7fac8037f61af32acc9c70c782db4a34133afd85
meet <23bca301@vtcbcsr.edu.in>
2026-08-24 18:58:02 +0530

    print 1-5

---
+++ 01-java-core/src/loop/Print1_5.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class Print1_5 {
+	public static void main(String[] args) {
+		
+	int a=1;
+	int b=5;
+	
+	do {
+		System.out.println(a);
+		a++;
+		
+	}while(a<=5);
+
+
+}
+}
