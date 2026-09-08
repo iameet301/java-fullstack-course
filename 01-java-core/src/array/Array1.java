73bcb85683182a0a0b4c88f21da0937cf6779e25
meet <23bca301@vtcbcsr.edu.in>
2026-09-08 15:40:19 +0530

    1to5

--- 01-java-core/src/array/Array1.java
+++ 01-java-core/src/array/Array1.java
@@ -11,7 +11,7 @@ public class Array1 {
 	    for(int i=0; i<arr.length; i++) {
 	    		System.out.println(arr[i]);
 	    		
-	    		//---------------------
+	    		//----------------------
 	    		
 	    }
 	    
