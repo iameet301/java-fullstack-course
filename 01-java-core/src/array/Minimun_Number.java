0278739df82602f7005b2ada2f742aa4bb97181d
meet <23bca301@vtcbcsr.edu.in>
2026-09-08 18:20:55 +0530

    minimum number

---
+++ 01-java-core/src/array/Minimun_Number.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Minimun_Number {
+	public static void main(String[] args) {
+		int[] a= {0,1,2,3,4,5};
+		
+		int min=a[0];
+		
+		for (int i = 1; i < a.length; i++) {
+		    if (a[i] < min) {
+		        min = a[i];
+		    }
+		}
+		System.out.println("minimum number "+min);
+	}
+
+}
