880ec7fb317dd2f0557ab9f928e2e72ef3648211
meet <23bca301@vtcbcsr.edu.in>
2026-09-14 18:55:07 +0530

    sort colors

---
+++ 01-java-core/src/array/SortColors.java
@@ -0,0 +1,31 @@
+package array;
+
+import java.util.Arrays;
+
+public class SortColors {
+    public static void main(String[] args) {
+        int[] arr = {2, 0, 2, 1, 1, 0};
+
+        int low = 0, mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
