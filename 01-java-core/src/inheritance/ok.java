3ef5be1f405650dceec7d389c2b43bf7ae77a898
meet <23bca301@vtcbcsr.edu.in>
2026-09-15 19:29:00 +0530

    realtion main

---
+++ 01-java-core/src/inheritance/ok.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class ok {
+	public static void main(String[] args) {
+		Parent p=new Parent();
+		p.getParentSaving();
+		
+		Child1 c=new Child1();
+		c.getParentSaving();
+		
+		Child2 c2=new Child2();
+		c2.getChild2Saving();
+				
+	}
+}
