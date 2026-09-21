252e69eee95eb62f7e73dfc0878e0c022db3f3cf
meet <23bca301@vtcbcsr.edu.in>
2026-09-21 18:36:06 +0530

    MINIMUM NUMBER

--- 01-java-core/src/array/Minimun_Number.java
+++ 01-java-core/src/array/Minimun_Number.java
@@ -4,9 +4,9 @@ public class Minimun_Number {
 	public static void main(String[] args) {
 		int[] a= {0,1,2,3,4,5};
 		
-		int min=a[0];
+		int min=a[1];
 		
-		for (int i = 1; i < a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 		    if (a[i] < min) {
 		        min = a[i];
 		    }
