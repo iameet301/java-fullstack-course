a6a72bc75dd95a95de77c6e638b8f47c24cfa2a3
meet <23bca301@vtcbcsr.edu.in>
2026-09-24 14:32:15 +0530

    bank

---
+++ 01-java-core/src/inheritance/Bank.java
@@ -0,0 +1,35 @@
+package inheritance;
+
+class Bank {
+    double getInterestRate() {
+        return 0.0;
+    }
+}
+
+class SBI extends Bank {
+    @Override
+    double getInterestRate() {
+        return 6.5;
+    }
+}
+
+class HDFC extends Bank {
+    @Override
+    double getInterestRate() {
+        return 7.2;
+    }
+
+
+public class PolymorphismDemo {
+    public static void main(String[] args) {
+        // Parent class reference, Child class object (Upcasting)
+        Bank bank;
+
+        bank = new SBI();
+        System.out.println("SBI Interest Rate: " + bank.getInterestRate() + "%");
+
+        bank = new HDFC();
+        System.out.println("HDFC Interest Rate: " + bank.getInterestRate() + "%");
+    }
+}
+}
