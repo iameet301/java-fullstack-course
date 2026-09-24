ffe2075ffb50f494bded52ab91532f38016a3742
meet <23bca301@vtcbcsr.edu.in>
2026-09-24 14:47:20 +0530

    multiple inheritance

---
+++ 01-java-core/src/inheritance/MultipleInheritanceDemo.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class MultipleInheritanceDemo {
+    public static void main(String[] args) {
+        Document doc = new Document();
+        doc.print();
+        doc.show();
+    }
+}
